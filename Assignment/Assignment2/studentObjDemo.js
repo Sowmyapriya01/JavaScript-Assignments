2c3baaccd3535b74de4a72191bdc0a59568c53a3
Sowmya Priya <sowmyapriya01@gmail.com>
2022-08-04 19:11:06 +0530

    Student Object with TeacherName

---
+++ Assignment/Assignment2/studentObjDemo.js
@@ -0,0 +1,12 @@
+let student = {
+  Name: "Sowmya Priya",
+  CollegeName: "iNeuron",
+  Location: "Bengaluru",
+  Pincode: 345654,
+  teacher: {
+    teacherName: "Mukesh Otwani",
+    teacherPhone: 9999999999,
+    teacherSubjects: "JavaScript",
+  },
+};
+console.log("Student Name is ",student.Name,". Teacher Name is ",student.teacher.teacherName );
